843c1f5e2ab2325b4da0fe1202aaafcd4bf6bf98
Rohith Viswanathan <rohithviswanathan3009@gmail.com>
2026-09-08 17:52:08 +0530

    removed settings

--- airscope/src/lib/navigation.ts
+++ airscope/src/lib/navigation.ts
@@ -3,7 +3,6 @@ import {
   ChartLineData01Icon,
   Home01Icon,
   Location01Icon,
-  Settings01Icon,
 } from "@hugeicons/core-free-icons";
 
 export const mainNavigation = [
@@ -26,12 +25,5 @@ export const mainNavigation = [
     label: "Locations",
     path: "/locations",
     icon: Location01Icon,
-  },
-];
-
-export const utilityNavigation = [
-  {
-    label: "Settings",
-    icon: Settings01Icon,
   },
 ];
