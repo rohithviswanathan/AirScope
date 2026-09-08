108b5731b21f364e792faf32c3955447f2f68363
Rohith Viswanathan <rohithviswanathan3009@gmail.com>
2026-09-08 12:51:54 +0530

    Integrated queryclientprovider

--- airscope/src/main.tsx
+++ airscope/src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 import "./index.css";
 
 import { ThemeProvider } from "./components/theme/ThemeProvider";
+import { queryClient } from "./lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
